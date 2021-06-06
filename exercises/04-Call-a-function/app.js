67f047f14cbf642d5ed05c05dd8bee934ea2bc0b
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-06 12:00:17 +0000

    HelloWorld

--- exercises/04-Call-a-function/app.js
+++ exercises/04-Call-a-function/app.js
@@ -3,7 +3,9 @@ function calculateArea(length,edge)
 	return length * edge;
 }
 // Your code Below this line:
-
+let squareArea2 = calculateArea(4,4);
+let squareArea3 = calculateArea(2,2);
+let squareArea4 = calculateArea(5,5);
 
 
 
--- exercises/05-Defining-vs-Calling-a-function/app.js
+++ exercises/05-Defining-vs-Calling-a-function/app.js
@@ -1,5 +1,6 @@
 //Define the function called "multi" that expects 2 parameters:
-
+function multi(localnumber1, localnumber2)
+{let returnValue = localnumber1*localnumber2;}
 
   //don't edit anything below this line
   var returnValue = multi(7,53812212);
